638b8988125d1eb8706059c0311bebb9574ee4a5
J. Vandenildo <jvandenildo@gmail.com>
2024-11-14 09:13:14 -0300

    aula 03

--- Lógica com JavaScript/app.js
+++ Lógica com JavaScript/app.js
@@ -1,11 +1,24 @@
 alert("Bem-vindo ao jogo de adivinhação");
 let numeroSecreto = 8;
 console.log(numeroSecreto);
-let chute = prompt("Insira um número entre 1 e 30!");
+let chute;
+let tentativas = 1;
 
-// se o chute for igual ao número secreto
-if (numeroSecreto == chute) {
-	alert(`Você acertou o número secreto ${numeroSecreto}`);
-} else {
-	alert("Você errou!");
+// enquanto o chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+	chute = prompt("Insira um número entre 1 e 30!");
+
+	// se o chute for igual ao número secreto
+	if (numeroSecreto == chute) {
+		alert(
+			`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas`
+		);
+	} else {
+		if (chute > numeroSecreto) {
+			alert(`O número secreto é menor que ${chute}.`);
+		} else {
+			alert(`O número secreto é maior que ${chute}.`);
+		}
+		tentativas++;
+	}
 }
---
+++ Lógica com JavaScript/aula03.js
@@ -0,0 +1,28 @@
+// primeiro desafio
+let contador = 1;
+while (contador <= 10) {
+	console.log(contador);
+	contador++;
+}
+
+// segundo desafio
+contador = 10;
+while (contador >= 0) {
+	console.log(contador);
+	contador--;
+}
+
+// terceiro desafio
+contador = prompt("Digite o número:");
+while (contador >= 0) {
+	console.log(contador);
+	contador--;
+}
+
+// quarto desafio
+contador = prompt("Digite o número:");
+let cont0 = 0;
+while (contador != cont0) {
+	console.log(cont0);
+	cont0++;
+}
